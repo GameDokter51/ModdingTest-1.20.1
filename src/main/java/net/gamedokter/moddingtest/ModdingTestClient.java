f884ea2a1f2830df678fa6de1c7aa49c6e03ba9d
GameDokter51 <fakemcfakeson0@gmail.com>
2024-03-20 17:59:34 -0400

    Again, please work

---
+++ src/main/java/net/gamedokter/moddingtest/ModdingTestClient.java
@@ -0,0 +1,10 @@
+package net.gamedokter.moddingtest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModdingTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
